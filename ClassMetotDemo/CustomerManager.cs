916f6389c84143c3cfbc1585328a108dbd2025e4
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2022-10-01 20:44:56 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine($"{customer.FirstName} {customer.LastName} isim müşteri eklendi.\n");
+        }
+
+        public void UpdateAddress(Customer customer, string address)
+        {
+            Console.WriteLine($"{customer.FirstName} {customer.LastName} isim müşterinin adresi {address} olarak güncellendi.\n");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine($"{customer.FirstName} {customer.LastName} isim müşteri silindi.\n");
+        }
+
+        public void Get(Customer customer)
+        {
+            Console.WriteLine($"Id: {customer.Id}");
+            Console.WriteLine($"İsim: {customer.FirstName}");
+            Console.WriteLine($"Soyisim: {customer.LastName}");
+            Console.WriteLine($"Adres: {customer.Address}");
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassMetotDemo;
+
+CustomerManager customerManager = new CustomerManager();
+
+Customer customer = new Customer()
+{
+    Id = 1,
+    FirstName = "Yusufcan",
+    LastName = "Adıgüzel",
+    Address = "İstanbul"
+};
+
+customerManager.Add(customer);
+
+customerManager.Delete(customer);
+
+customerManager.UpdateAddress(customer, "Ankara");
+
+customerManager.Get(customer);
+
